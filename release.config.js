b3dfcf1b924cbba64151914090f6656c91b5d0d4
Matthias Günter <matzeeable@gmail.com>
2019-10-11 09:41:03 +0200

    fix: correctly push via semantic-release in deploy stage

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 const releaseBranch = "master",
     branch = process.env.BRANCH || releaseBranch,
+    prepareOnly = +process.env.PREPARE_ONLY === 1,
     commitAnalyzer = "@semantic-release/commit-analyzer",
     releaseNotesGenerator = [
         "@semantic-release/release-notes-generator",
@@ -14,48 +15,48 @@ const releaseBranch = "master",
     changeLog = "@semantic-release/changelog",
     execYarnVersion = "yarn version --no-git-tag-version --new-version ${nextRelease.version}";
 
-module.exports =
-    branch === releaseBranch
-        ? {
-              // Main release branch
-              branch: branch,
-              plugins: [
-                  commitAnalyzer,
-                  releaseNotesGenerator,
-                  changeLog,
-                  [
-                      "@semantic-release/exec",
-                      {
-                          prepareCmd: execYarnVersion
-                      }
-                  ],
-                  [
-                      "@semantic-release/gitlab",
-                      {
-                          assets: [{ path: "dist/*.zip", label: "Installable plugin" }],
-                          assets: [{ path: "docs", label: "Technical documentation" }]
-                      }
-                  ]
+module.exports = !prepareOnly
+    ? {
+          // Main release branch
+          branch: branch,
+          plugins: [
+              commitAnalyzer,
+              releaseNotesGenerator,
+              changeLog,
+              [
+                  "@semantic-release/exec",
+                  {
+                      prepareCmd: execYarnVersion
+                  }
+              ],
+              "@semantic-release/git",
+              [
+                  "@semantic-release/gitlab",
+                  {
+                      assets: [{ path: "dist/*.zip", label: "Installable plugin" }],
+                      assets: [{ path: "docs", label: "Technical documentation" }]
+                  }
               ]
-          }
-        : {
-              // Steps to get the current version, BRANCH=develop yarn semantic-release
-              // See also https://git.io/Jelnu
-              branch: branch,
-              ci: false,
-              plugins: [
-                  commitAnalyzer,
-                  releaseNotesGenerator,
-                  changeLog,
-                  [
-                      "@semantic-release/exec",
-                      {
-                          prepareCmd: [
-                              execYarnVersion,
-                              "echo Exit with error due to prevent publishing here",
-                              "exit 1" // Always exit so no publish takes place
-                          ].join(" && ")
-                      }
-                  ]
+          ]
+      }
+    : {
+          // Steps to get the current version, BRANCH=develop yarn semantic-release
+          // See also https://git.io/Jelnu
+          branch: branch,
+          ci: false,
+          plugins: [
+              commitAnalyzer,
+              releaseNotesGenerator,
+              changeLog,
+              [
+                  "@semantic-release/exec",
+                  {
+                      prepareCmd: [
+                          execYarnVersion,
+                          "echo Exit with error due to prevent publishing here",
+                          "exit 1" // Always exit so no publish takes place
+                      ].join(" && ")
+                  }
               ]
-          };
+          ]
+      };
